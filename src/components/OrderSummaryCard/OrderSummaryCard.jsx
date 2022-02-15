e8773036464d111fa24b75a11bc3189a4fd6a3fb
AliDayi97 <alidayi97@hotmail.com.tr>
2022-02-15 23:13:32 +0000

    disabled eslint anchor-is-valid

--- src/components/OrderSummaryCard/OrderSummaryCard.jsx
+++ src/components/OrderSummaryCard/OrderSummaryCard.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import musicIcon from "./../../assets/order-summary-component/images/icon-music.svg";
 import hero from "./../../assets/order-summary-component/images/illustration-hero.svg";
